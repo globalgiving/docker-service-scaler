09ca3aeda2c11401376feda820373b30ee45e3c9
Justin Rupp <jrupp@globalgiving.org>
2017-08-30 14:28:05 -0400

    Adding networks and constraints, changing ports to list

--- scaler.sh
+++ scaler.sh
@@ -1,14 +1,15 @@
 #!/bin/bash - 
 
 DATA="$(aws dynamodb scan --region "${AWS_REGION}" --table-name "${DYNAMODB_TABLE}")"
-for I in `echo ${DATA} | jq -rc '.Items[] | {name: .name.S, replicas: .replicas.N, mount: .mount.S, mode: .mode.S, image: .image.S, network: .network.S, ports: .ports.S | split(","), memory: .memory.N, cpu: .cpu.N, image_private: .image_private.BOOL | tostring}'`;
+for I in `echo ${DATA} | jq -rc '.Items[] | {name: .name.S, replicas: .replicas.N, mount: .mount.S, mode: .mode.S, image: .image.S, networks: [.networks.L[] | .S], ports: [.ports.L[] | .S], memory: .memory.N, cpu: .cpu.N, constraint: .constraint.S, image_private: .image_private.BOOL | tostring}'`;
 do
   SERVICE_NAME="$(echo "$I" | jq -r '.name')"
   SERVICE_REPLICAS="$(echo "$I" | jq -r '.replicas')"
   EXISTS=$(docker service ls --filter "name=${SERVICE_NAME}" --format "{{.ID}}" | wc -l)
   if [[ "${EXISTS}" = "0" ]]
   then
-    SERVICE_NETWORK="$(echo "$I" | jq -r '.network')"
+    SERVICE_NETWORKS="$(echo "$I" | jq -r '.networks[]')"
+    SERVICE_CONSTRAINT="$(echo "$I" | jq -r '.constraint')"
     SERVICE_IMAGE="$(echo "$I" | jq -r '.image')"
     SERVICE_IMAGE_PRIVATE="$(echo "$I" | jq -r '.image_private')"
     SERVICE_MODE="$(echo "$I" | jq -r '.mode')"
@@ -20,36 +21,51 @@ do
     REGISTRY_AUTH=""
     RESERVE_MEMORY=""
     RESERVE_CPU=""
+    CONSTRAINT=""
+    NETWORKS=""
+
     if [[ "${SERVICE_MOUNT}" = "none" ]]
     then
       SERVICE_MOUNT=""
     else
       SERVICE_MOUNT="--mount ${SERVICE_MOUNT}"
     fi
-    if [[ "${SERVICE_PORTS}" = "none" ]]
-    then
-      PUBLISH=""
-    else
-      for P in `echo ${SERVICE_PORTS}`;
-      do
-        PUBLISH="--publish $P ${PUBLISH}"
-      done
-    fi
+
+    for P in `echo ${SERVICE_PORTS}`;
+    do
+      PUBLISH="--publish $P ${PUBLISH}"
+    done
+
+    for N in `echo ${SERVICE_NETWORKS}`;
+    do
+      NETWORKS="--network $N ${NETWORKS}"
+    done
+
     if [[ "${SERVICE_IMAGE_PRIVATE}" == "true" ]]
     then
       REGISTRY_AUTH="--with-registry-auth"
     fi
+
     if ! [[ "${SERVICE_MEMORY}" = "0" ]]
     then
       RESERVE_MEMORY="--reserve-memory ${SERVICE_MEMORY}"
     fi
+
     if ! [[ "${SERVICE_CPU}" = "0" ]]
     then
       RESERVE_CPU="--reserve-cpu ${SERVICE_CPU}"
     fi
+
+    if ! [[ "${SERVICE_CONSTRAINT}" = "none" ]]
+    then
+      CONSTRAINT="--constraint \"${SERVICE_CONSTRAINT}\""
+    fi
+
     echo "Creating ${SERVICE_NAME}"
-    docker service create --name "${SERVICE_NAME}" --replicas "${SERVICE_REPLICAS}" ${RESERVE_MEMORY} ${RESERVE_CPU} --constraint "engine.labels.network==${SERVICE_NETWORK}" --placement-pref 'spread=engine.labels.availability_zone' --mode "${SERVICE_MODE}" ${SERVICE_MOUNT} ${PUBLISH} ${REGISTRY_AUTH} ${SERVICE_IMAGE}
+    docker service create --name "${SERVICE_NAME}" --replicas "${SERVICE_REPLICAS}" ${RESERVE_MEMORY} ${RESERVE_CPU} ${CONSTRAINT} --placement-pref 'spread=engine.labels.availability_zone' --mode "${SERVICE_MODE}" ${SERVICE_MOUNT} ${PUBLISH} ${NETWORKS} ${REGISTRY_AUTH} ${SERVICE_IMAGE}
   else
     docker service scale "${SERVICE_NAME}=${SERVICE_REPLICAS}"
   fi
 done
+
+# TODO: docker service inspect NAME | jq -r '.[] | .Spec | {name: .Name, replicas: .Mode.Replicated.Replicas, mount: .sdf, mode: .Mode | keys[] | ascii_downcase, image: .TaskTemplate.ContainerSpec.Image | split("@")[0]}'
