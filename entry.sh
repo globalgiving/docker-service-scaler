4f63cb113e7f6ecd0fd3a0cdc3154e959374a215
Justin Rupp <jrupp@globalgiving.org>
2017-08-30 22:37:29 -0400

    Adding dockerhub login from provided secret

--- entry.sh
+++ entry.sh
@@ -7,6 +7,10 @@ echo "Initialize logging for scaler daemon"
 # setup symlink to output logs from relevant scripts to container logs
 ln -s /proc/1/fd/1 /var/log/docker/scaler.log
 
+echo "Logging into DockerHub"
+source /run/secrets/dockerhub
+docker login -u "${DOCKERHUB_USER}" -p "${DOCKERHUB_PASS}"
+
 # start cron
 /usr/sbin/crond -f -l 9 -L /var/log/cron.log
 
